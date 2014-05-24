142bd63208b303600d2052a59179e8b2d3e52da3
unknown <anarbek@gmail.com>
2014-05-24 14:16:35 +0300

    test

--- cms/tests/static_analysis.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-from cms.test_utils.util.static_analysis import pyflakes
-
-
-class StaticAnalysisTest(TestCase):
-    def test_pyflakes(self):
-        self.assertEqual(pyflakes(), 0)
