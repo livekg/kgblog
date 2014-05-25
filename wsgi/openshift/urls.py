e48a8438e9ab97477df0dbd947b38ee0d408c360
Anarbek <anarbek@gmail.com>
2014-05-25 14:13:31 +0300

    media

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -8,6 +8,9 @@ admin.autodiscover()
 urlpatterns = i18n_patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cms.urls')),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    url(r'', include('django.contrib.staticfiles.urls')),
 )
 
 if settings.DEBUG:
